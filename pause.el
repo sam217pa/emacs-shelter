4a4b8e5e1715949c076caf2f3658742e6f597e1b
sam217pa <samuel.barreto8@gmail.com>
2019-02-22 09:24:00 +0100

    fix: pause is self-dependent

--- pause.el
+++ pause.el
@@ -25,30 +25,41 @@
 ;;; Code:
 
 (require 'cl-lib)
-(require 'camp-utils)
+
+(defun pause--group (source n)
+  "Divide SOURCE list in N groups and stack together the last
+elements."
+  (if (zerop n) (error "Zero length"))
+  (cl-labels ((rec (source acc)
+                   (let ((rest (nthcdr n source)))
+                     (if (consp rest)
+                         (rec rest (cons (cl-subseq source 0 n) acc))
+                       (nreverse (cons source acc))))))
+    (if source (rec source nil) nil)))
 
 ;;;###autoload
 (defun pause-prompt (prompt)
-    "Add text properties to special cookies in PROMPT.
+  "Add text properties to special cookies in PROMPT.
 
 Highlight text between square brackets with `highlight',
 between round brackets with `font-lock-builtin-face',
 between curly brackets with `font-lock-constant-face'."
-    (let ((str prompt)
-          (regexp-colors '(("\\[[-_A-Za-z0-9=]+\\]" . 'highlight)
-                           ("([-_A-Za-z0-9=]+)"     . 'font-lock-builtin-face)
-                           ("{[-_A-Za-z0-9=]+}"     . 'font-lock-constant-face))))
-      (cl-loop
-       for re-color in regexp-colors
-       do (let ((start 0))
-            (while (string-match (car re-color) str start)
-              (let ((beg (1+ (match-beginning 0)))
-                    (end (1- (match-end 0))))
-                (setq start end)
-                (add-text-properties
-                 beg end `(face ,(cdr re-color))
-                 str)))))
-      str))
+  (declare (doc-string 1))
+  (let ((str prompt)
+        (regexp-colors '(("\\[[-_A-Za-z0-9=]+\\]" . 'link)
+                         ("([-_A-Za-z0-9=]+)"     . 'highlight)
+                         ("{[-_A-Za-z0-9=]+}"     . 'font-lock-constant-face))))
+    (cl-loop
+     for re-color in regexp-colors
+     do (let ((start 0))
+          (while (string-match (car re-color) str start)
+            (let ((beg (1+ (match-beginning 0)))
+                  (end (1- (match-end 0))))
+              (setq start end)
+              (add-text-properties
+               beg end `(face ,(cdr re-color))
+               str)))))
+    str))
 
 (defmacro pause--ov-puts (ov &rest args)
   "Put ARGS properties to overlay OV.
@@ -57,10 +68,34 @@ ARGS should be grouped by two. Wraps `overlay-put'."
   (declare (indent 2))
   `(progn
      ,@(mapcar (lambda (props) `(overlay-put ,ov ,@props))
-               (camp--group args 2))))
+               (pause--group args 2))))
 
 (defmacro pause (persist-p prompt &rest args)
-  (declare (indent defun))
+  "Put game in pause with a transient map.
+
+This macro defines a transient keymap (see `set-transient-map')
+with an overlay popup describing the available commands. The
+overlay is put directly below point.
+
+  (pause t
+    \"Line spacing:\nIncrease [+] / Decrease [-]\"
+    \"+\" (cl-incf line-spacing)
+    \"-\" (cl-decf line-spacing))
+
+In this exemple a transient map is defined, documented on the
+second line. Third and fourth line maps + and - to a command that
+increase the line-spacing. Any other key escape the transient map
+and remove the popup.
+
+PERSIST-P should be set to t if the map is to stay active after
+a key present in the keymap has been pressed.
+
+PROMPT is a message prompted out in an overlay below point.
+
+ARGS is a list of key and the command they trigger. Commands are
+wrapped into a (lambda () (interactive)) form, so that
+non-interactive functions are directly called."
+  (declare (indent defun) (doc-string 2))
   (let ((ov         (gensym "pause-ov-"))
         (ct         (gensym "pause-ct-"))
         (kmp        (gensym "pause-kmp-"))
@@ -86,7 +121,7 @@ ARGS should be grouped by two. Wraps `overlay-put'."
                 (pause--ov-puts ,ov
                     'display str
                     'before-string "\n"
-                    'after-string ""
+                    'after-string "\n"
                     'priority 9999)
                 nil))
             ;; pad string with empty char on the left so that it appears
@@ -102,7 +137,7 @@ ARGS should be grouped by two. Wraps `overlay-put'."
             for kf in (sam--group args 2)
             collect `(define-key ,kmp (kbd ,(car kf))
                        (lambda () (interactive) ,(cadr kf))))
-         (,ov-display (,ov-pad (pause-prompt ,prompt)))
+         (,ov-display (,ov-pad ,prompt))
          (set-transient-map ,kmp ,persist-p #',ov-cleanup)
          (message "...")))))
 
